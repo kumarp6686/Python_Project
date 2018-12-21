392aa7f1b69cc6e759696ede0801a880c398e376
kumarp6686 <pakumar.sf@gmail.com>
2018-12-20 21:34:17 -0500

    updates

---
+++ FirstPythonCode.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+
+# Comments :
+
+	# 1. Single Line : 1. '', 2. " ", 3. ''' ''', 4. """ """ & 5. # 
+	# 2. Multi Line  : 1. ''' ''' & 2. """ """
+
+"Notes" 
+
+print("Hi, Welcome to Python World")
+
+print("Today is our Day-1 Demo")
+
+github_username = 'keshavkummari'
+linkedin_username = 'keshavkummari'
+gmail_id = 'keshav.kummari@gmail.com'
+
+print(github_username)
+print(linkedin_username)
+print(gmail_id)
+
+
+
+
+
